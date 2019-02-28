1ecb532cf2bb4e7f9e21fb429782d333b0b61790
havt <hai.v@shopstack.asia>
2019-02-28 13:46:37 +0700

    unit4: update

--- unit4/src/components/about/about.js
+++ unit4/src/components/about/about.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class About extends Component {
+     constructor() {
+    super();
+    this.state = {
+      old: 31,
+    };
+  }
+
+  updateOld() {
+    this.setState((prevState, props) => {
+      return { old: prevState.old + 1 }
+    });
+  }
     render() {
         return (
             <div class="Aboutinfo">
                 <p>FullName: {this.props.fullname}</p>
                 <p>Email: {this.props.email}</p>
-                <p>Old: {this.props.old}</p>
+                <p><span onClick={() => this.updateOld()}>Old</span>: {this.state.old}</p>
                 <p>Gender: {this.props.gender}</p>
             </div>
         )
@@ -16,7 +28,6 @@ class About extends Component {
 About.defaultProps = {
     fullname: 'Hai Vu',
     email: 'hai.v@shopstack.asia',
-    old: '32',
     gender: 'female'
 }
 
