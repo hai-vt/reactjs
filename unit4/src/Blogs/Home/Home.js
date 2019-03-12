b2c628a39f8d353b03fe1f461844a343eae4ad44
havt <hai.v@shopstack.asia>
2019-03-12 13:13:08 +0700

    create router for blog app

--- unit4/src/Blogs/Home/Home.js
+++ unit4/src/Blogs/Home/Home.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './home.css';
 
 export default class Home extends Component {
     render () {
         return (
-            <Router>
             <div className="home-content">
                 <div className="welcome">Hello every one</div>
                 <div className="introduce">
                     I am Hai, I am from VietNam
                 </div>
                 <div className="link">
-                    For more details please click <Link to="/about/">here</Link>
+                    For more details please <Link to="/about/">click here</Link>
                 </div>
             </div>
-        </Router>
         )
     }
 }
